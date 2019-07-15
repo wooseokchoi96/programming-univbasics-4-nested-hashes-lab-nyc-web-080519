f56caf6dd8477ea7fcc97d4bd13c5f359e953b87
Andy Choi <wooseokchoi96@gmail.com>
2019-07-15 21:24:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +82,8 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash = {yukihiro_matsumoto: {known_for: "Ruby", languages: ["LISP", "C"]} }.merge(programmer_hash)
+  programmer_hash.first
 end
 
 def changing_alan
@@ -121,5 +124,4 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
 end
